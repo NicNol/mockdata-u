4e0f6fd28ac177e8604e437372c2deb4eac8d37d
AloofSkip <nic.veloce@gmail.com>
2022-05-04 21:00:21 -0700

    canvas to png export pipeline working

--- pages/Canvas.js
+++ pages/Canvas.js
@@ -1,15 +1,13 @@
 /* 
     Adopted from: https://code.tutsplus.com/tutorials/how-to-draw-a-pie-chart-and-doughnut-chart-using-javascript-and-html5-canvas--cms-27197
+    and https://flaviocopes.com/canvas-node-generate-image/
 */
 
-import React, { useEffect, useRef } from "react";
-import { Box } from "@chakra-ui/react";
+import React from "react";
+const { createCanvas } = require("canvas");
+const fs = require("fs");
 
 export default function Canvas(props) {
-    const canvasWidth = 800;
-    const canvasHeight = 800;
-    const canvasRef = useRef(null);
-    const legendRef = useRef(null);
     const userData = {
         "Classical music": 10,
         "Alternative rock": 14,
@@ -17,6 +15,15 @@ export default function Canvas(props) {
         Jazz: 12,
         Emo: 5,
     };
+
+    // Set Dimensions of Canvas
+    const borderWidth = 64;
+    const pieDiameter = 800;
+    const pieLegendMargin = 20;
+    const legendHeight = 50 * Object.keys(userData).length;
+    const canvasWidth = borderWidth * 2 + pieDiameter;
+    const canvasHeight =
+        borderWidth * 2 + pieDiameter + pieLegendMargin + legendHeight;
 
     const grandTotal = Object.values(userData).reduce(
         (prev, curr) => prev + curr,
@@ -65,7 +72,7 @@ export default function Canvas(props) {
     }
 
     function createSlices(context) {
-        const radius = Math.min(canvasWidth / 2, canvasHeight / 2);
+        const radius = Math.min(pieDiameter / 2, pieDiameter / 2);
         const labels = [];
         const output = [];
         let startAngle = 0;
@@ -73,22 +80,23 @@ export default function Canvas(props) {
         Object.keys(userData).forEach((key, index) => {
             const sliceAngle = (2 * Math.PI * userData[key]) / grandTotal;
             const labelX =
-                canvasWidth / 2 +
+                pieDiameter / 2 +
+                borderWidth +
                 (radius / 2) * Math.cos(startAngle + sliceAngle / 2);
             const labelY =
-                canvasHeight / 2 +
+                pieDiameter / 2 +
+                borderWidth +
                 (radius / 2) * Math.sin(startAngle + sliceAngle / 2);
 
             drawSlice(
                 context,
-                canvasWidth / 2,
-                canvasHeight / 2,
+                pieDiameter / 2 + borderWidth,
+                pieDiameter / 2 + borderWidth,
                 radius,
                 startAngle,
                 startAngle + sliceAngle,
                 colors[index]
             );
-            console.log(radius);
             labels.push({
                 objectKey: key,
                 positionX: labelX,
@@ -104,37 +112,39 @@ export default function Canvas(props) {
         return output;
     }
 
-    function createLegend(categories) {
-        const legend = legendRef.current;
-        const HTML = categories.reduce(
-            (prev, curr) => `${prev}
-                <div>
-                    <span style='display:inline-block;width:40px;height:40px;background-color:${curr.color};'>
-                        &nbsp;
-                    </span>
-                    <span style='font-size:24px;position:relative;top:10px;'>${curr.objectKey}</span>
-                </div>`,
-            ""
-        );
-        legend.innerHTML = HTML;
+    function createLegend(context, categories) {
+        const squareLength = 40;
+        const squareMargin = 10;
+        let currentY = borderWidth + pieDiameter + pieLegendMargin;
+        categories.forEach((category) => {
+            // Draw Square
+            context.fillStyle = category.color;
+            context.fillRect(borderWidth, currentY, squareLength, squareLength);
+
+            // Add Text
+            context.fillStyle = "#000000"; // Black
+            context.font = "24px Arial";
+            context.fillText(
+                category.objectKey,
+                borderWidth + squareLength + squareMargin,
+                currentY + 28
+            );
+
+            // Move Y pointer
+            currentY += squareLength + squareMargin;
+        });
     }
 
     function createPieChart() {
-        const canvas = canvasRef.current;
+        const canvas = createCanvas(canvasWidth, canvasHeight);
         const context = canvas.getContext("2d");
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-
         const categories = createSlices(context);
-        createLegend(categories);
+        createLegend(context, categories);
+        const out = fs.createWriteStream("./public/pc/image.png");
+        const stream = canvas.createPNGStream();
+        stream.pipe(out);
+        out.on("finish", () => console.log("The PNG file was created."));
     }
 
-    useEffect(createPieChart, []);
-
-    return (
-        <Box m="16">
-            <canvas ref={canvasRef}></canvas>
-            <div ref={legendRef}></div>
-        </Box>
-    );
+    return createPieChart();
 }
