91457cc215bbc5bbd7ea7b7f52ba55d5ada96116
AloofSkip <nic.veloce@gmail.com>
2022-05-14 11:25:54 -0700

    create script to download images from imageLinks.txt

---
+++ scraper/saveImages.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const request = require("request");
+
+const imageFile = "./scraper/imageLinks.txt";
+
+fs.readFile(imageFile, "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const textFileLines = data.split("\r\n");
+    textFileLines.pop(); // remove last element (empty string)
+    textFileLines.forEach((line) => saveImage(line));
+});
+
+function saveImage(textLine) {
+    const [filename, url] = textLine.split(": ");
+    const fileLocation = `./public/images/${filename}.${getFileExtension(url)}`;
+    downloadImage(url, fileLocation, () => null);
+}
+
+const downloadImage = (imageURL, filename, cb) => {
+    request.head(imageURL, (err, res, body) => {
+        request(imageURL).pipe(fs.createWriteStream(filename)).on("close", cb);
+    });
+};
+
+function getFileExtension(fileName) {
+    const pattern = /(?:\.([^.]+))?$/;
+    const fileExtension = pattern.exec(fileName)[1];
+    return fileExtension;
+}
